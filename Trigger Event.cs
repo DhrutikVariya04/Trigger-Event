2816a333698b83f1755ce882c3a24965b0d8e6b8
Dhrutik Variya <dhrutikv@gmail.com>
2024-10-19 17:58:13 +0530

    Version(1.02)

--- Trigger Event.cs
+++ Trigger Event.cs
@@ -1,21 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UIElements;
 
 public class TriggerEvent : MonoBehaviour
 {
     [SerializeField]
     GameObject Box;
 
+    PointerEventData eventData;
     private Vector3 startPosition;
-    private float speed = 10f;
+    private Vector2 initialPosition;
+    private float speed = 100f;
+    private bool isDragging;
 
     private void Start()
     {
-        startPosition = Box.transform.position;
+         startPosition = Box.transform.position;
     }
+
+    void Update()
+    {
+        if (isDragging)
+        {
+            Vector2 currentPosition = eventData.position;
+            Vector2 delta = currentPosition - initialPosition;
+
+            float maxDelta = Screen.height * 0.2f;
+            delta = Vector2.ClampMagnitude(delta, maxDelta);
+
+            Vector2 newPosition = ((Vector2)Box.transform.position + delta.normalized) * speed * Time.deltaTime;
+
+            newPosition.y = startPosition.y;
+
+            Box.transform.position = newPosition;
+
+        }
+    }
+
+    //For Drag Box :--
+
     public void Right()
     {
         var Pos = Box.transform.position;
@@ -58,15 +80,31 @@ public class TriggerEvent : MonoBehaviour
         Box.transform.position = Pos;
     }
 
+    
     public void OnDrag(BaseEventData Data)
     {
         PointerEventData eventData = Data as PointerEventData;
 
         Vector2 dragDelta = new Vector2(eventData.delta.x, 0) * Time.deltaTime;
-        dragDelta.x += Mathf.Clamp(dragDelta.x, -455f, 455);
         Box.transform.position += (Vector3)dragDelta;
 
         print("dragDelta ====> "+ dragDelta.x);
     }
 
+    // For Drag Box :--
+
+    public void OnPointerDown(BaseEventData eventData)
+    {
+        PointerEventData EventData = eventData as PointerEventData;
+
+        this.eventData = EventData;
+        isDragging = true;
+        initialPosition = EventData.position;
+    }
+
+    public void OnPointerUp(BaseEventData eventData)
+    {
+        PointerEventData EventData = eventData as PointerEventData;
+        isDragging = false;
+    }
 }
